6a38c8eedb4034685e320f0bc7683c25a36b3fc5
Rohittt01 <rohitrwt745@gmail.com>
2023-10-22 17:35:34 +0530

    add main.js

--- main.js
+++ main.js
@@ -6,6 +6,38 @@ button.addEventListener("click", function(event){
         event.preventDefault();
     }
     else{
-        
+       var newdiv = document.createElement("div")
+       newdiv.classList.add("todo-list")
+       var parentdiv = document.getElementsByClassName("todo")[0]
+       parentdiv.appendChild(newdiv)
+       var newdiv2 = document.createElement("div")
+       newdiv2.classList.add("todo-div")
+       newdiv.appendChild(newdiv2)
+       var h2title = document.createElement("h2")
+       h2title.textContent = title.value
+       newdiv2.appendChild(h2title)
+
+       var newdiv3 = document.createElement("div")
+       newdiv3.id = "button"
+       newdiv.appendChild(newdiv3)
+       var newdiv4 = document.createElement("div")
+       newdiv4.classList.add("button")
+       newdiv3.appendChild(newdiv4)
+       
+       var newbutton = document.createElement("button")
+       newbutton.textContent= "Completed"
+       newbutton.id = "complete"
+       newdiv4.appendChild(newbutton)
+       var newbutton1 = document.createElement("button")
+       newbutton1.textContent= "Update"
+       newbutton1.id = "update"
+       newdiv4.appendChild(newbutton1)
+       var newbutton2 = document.createElement("button")
+       newbutton2.textContent= "Delete"
+       newbutton2.id = "delete"
+       newdiv4.appendChild(newbutton2)
+
+       var newhr = document.createElement("hr")
+       parentdiv.appendChild(newhr)
     }
 })
