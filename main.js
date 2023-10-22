f5577bc9a01b1bda3a1e5a0e76fcec624a618017
Rohittt01 <rohitrwt745@gmail.com>
2023-10-22 12:35:04 +0530

    add needed styling

---
+++ main.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title")
+const button = document.getElementById("add-button")
+
+button.addEventListener("click", function(event){
+    if(title.value == ""){
+        event.preventDefault();
+    }
+    else{
+        
+    }
+})
